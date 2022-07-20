8fc01e1886d6dddaa47c6585e65d8a46385dc6f9
Lindy Bustabad <55560264+lkbbad@users.noreply.github.com>
2022-07-20 11:15:53 -0400

    typo 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,5 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruitvice_response = requests.get("http://fruityvice.com/api/fruit/watermelon")
+fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
